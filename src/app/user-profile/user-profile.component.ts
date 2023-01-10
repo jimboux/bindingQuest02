dc6543fd525dac7f2218c407fa33bd1426b21ad9
jimboux <100678513+jimboux@users.noreply.github.com>
2023-01-10 15:40:15 +0100

    first commit

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent {
+  user = {
+    name: 'Doe',
+    firstName: 'John',
+    age: 25,
+    quote: '',
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg',
+    pwet: true,
+  };
+
+  hideAge() {
+    if (this.user.pwet === false) {
+      this.user.pwet = true;
+    } else this.user.pwet = false;
+  }
+}
